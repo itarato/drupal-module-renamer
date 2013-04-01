4e29c6f2407d498f1b828a9a8d5c25b91a970367
Peter Arato <it.arato@gmail.com>
2013-04-01 01:39:52 +0100

    Prototype.

--- module_renamer.py
+++ module_renamer.py
@@ -28,15 +28,24 @@ class ModuleRenamer(object):
         file_path = os.path.join(dirpath, filename)
         new_file_path = os.path.join(dirpath, new_filename)
         shutil.move(file_path, new_file_path)
+        print('\033[37m[mv]\033[0m ' + file_path + ' -> ' + new_file_path)
         self.rename_strings(new_file_path)
 
   def rename_strings(self, filename):
     file = open(filename, 'r')
     content = file.read()
-    content = re.sub(self.from_name, self.to_name, content)
-    content = re.sub(cap(self.from_name), cap(self.to_name), content)
+    content = self.replace_string(self.from_name, self.to_name, content)
+    content = self.replace_string(cap(self.from_name), cap(self.to_name), content)
     file.close()
 
     file = open(filename, 'w')
     file.write(content)
     file.close()
+
+  def replace_string(self, pattern, to, text):
+    matches = re.findall('^.*' + pattern + '.*$', text, flags = re.M)
+    if matches:
+      for match in matches:
+        print('\033[37m[sub]\033[0m ' + re.sub(pattern, "\033[31m" + to + "\033[0m", match))
+    text = re.sub(pattern, to, text)
+    return text
--- rename.py
+++ rename.py
@@ -2,18 +2,16 @@ import sys
 from module_renamer import ModuleRenamer
 
 if __name__ == '__main__':
-  print("Drupal Module Renamer")
-
   if len(sys.argv) < 4:
     print("Argument error. Syntax:\n$ python -O rename.py NAME_FROM NAME_TO PATH_TO_MODULE")
     sys.exit(1)
 
-  print("Rename from " + sys.argv[1] + " to " + sys.argv[2] + " in " + sys.argv[3])
+  print("Rename from \033[35m" + sys.argv[1] + "\033[0m to \033[35m" + sys.argv[2] + "\033[0m in \033[35m" + sys.argv[3] + "\033[0m.")
 
   renamer = ModuleRenamer(sys.argv[1], sys.argv[2], sys.argv[3])
 
   if not renamer.validate_args():
-    print("Argument error.")
+    print("Argument error. Names should be valid PHP function literals. Path should exist.")
     sys.exit(1)
 
   renamer.rename()
